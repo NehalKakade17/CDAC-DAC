3b330d2be7116188da11722eaa03b77c05fd2887
Nehal <nehalkakade5@gmail.com>
2024-08-28 18:19:16 +0530

    Deletion check

--- Assignment-1-Cdac/areaSq.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class areaSq{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int l =sc.nextInt();
-        System.out.println(l*l);
-    }
-}
